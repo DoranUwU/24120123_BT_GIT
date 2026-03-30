517dc8e335081e0f2ef1ff3191444c9926859f4b
vly-us-ninjacoder2k6 <holy856452545@gmail.com>
2026-03-30 21:25:14 +0700

    update

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def print_hi(name):
 
     print(f'Hello {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 if __name__ == '__main__':
     print_hi('Github')
 
